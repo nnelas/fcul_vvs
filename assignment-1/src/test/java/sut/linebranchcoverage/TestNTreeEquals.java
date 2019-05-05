513d420b635bbfbdf1656e0e89f4e88a4317cd3e
Simao Ferreira <ferreira.sds@gmail.com>
2019-05-05 02:35:28 +0100

    Testes no equals mais completo, sim da erro num dos testes, ver se deixamos assim ou comentamos

--- assignment-1/src/test/java/sut/linebranchcoverage/TestNTreeEquals.java
+++ assignment-1/src/test/java/sut/linebranchcoverage/TestNTreeEquals.java
@@ -17,15 +17,53 @@ public class TestNTreeEquals {
 	 *     branch 1
 	 */
 
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	public void testNotSameObject() {
+		ArrayNTree<Integer> mArrayNTree = new ArrayNTree<Integer>(Arrays.asList(2, 3, 4), 2);		
+
+		assertFalse(mArrayNTree.equals(new String("Hello World")));
+	}
+	
 	@Test
 	public void testEqualsTree() {
 		ArrayNTree<Integer> mArrayNTree = new ArrayNTree<Integer>(Arrays.asList(2, 3, 4), 2);
-		ArrayNTree<Integer> mArrayNTree2 = new ArrayNTree<Integer>(Arrays.asList(3, 4), 2);
-		ArrayNTree<Integer> clone = mArrayNTree.clone(); 
+		ArrayNTree<Integer> mArrayNTree2 = new ArrayNTree<Integer>(Arrays.asList(2, 3, 4), 2);
 		
-		assertTrue(mArrayNTree.equals(mArrayNTree));
-		assertTrue(mArrayNTree.equals(clone));
-		assertFalse(mArrayNTree.equals(mArrayNTree2));
-		assertFalse(mArrayNTree.equals(new String("Hello World")));
+		assertTrue(mArrayNTree.equals(mArrayNTree2));
 	}
+
+	/*
+	 * Not same by elements but same capacity
+	 */
+	@Test
+	public void testNotEqualsTree1() {
+		ArrayNTree<Integer> mArrayNTree = new ArrayNTree<Integer>(Arrays.asList(2, 3, 4), 2);
+		ArrayNTree<Integer> mArrayNTree2 = new ArrayNTree<Integer>(Arrays.asList(3, 4), 2);
+		
+		assertFalse(mArrayNTree.equals(mArrayNTree2));
+	}
+	
+	/*
+	 * Same by elements but not same capacity
+	 */
+	@Test
+	public void testNotEqualsTree2() {
+		ArrayNTree<Integer> mArrayNTree = new ArrayNTree<Integer>(Arrays.asList(2, 3, 4), 2);
+		ArrayNTree<Integer> mArrayNTree2 = new ArrayNTree<Integer>(Arrays.asList(2, 3, 4), 3);
+		
+		assertFalse(mArrayNTree.equals(mArrayNTree2));
+	}
+	
+	/*
+	 * Not same by elements but not same capacity
+	 */
+	@Test
+	public void testNotEqualsTree3() {
+		ArrayNTree<Integer> mArrayNTree = new ArrayNTree<Integer>(Arrays.asList(2, 3, 4), 2);
+		ArrayNTree<Integer> mArrayNTree2 = new ArrayNTree<Integer>(Arrays.asList(3, 4), 3);
+		
+		assertFalse(mArrayNTree.equals(mArrayNTree2));
+	}
+
 }
